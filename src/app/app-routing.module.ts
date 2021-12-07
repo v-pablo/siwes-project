40fde9ba5e5479b241d0f03789b2557fdd321048
v-pablo <otitojuvictor9@gmail.com>
2021-12-07 17:49:50 +0100

    routed the attendance page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LecturerComponent } from './component/lecturer/lecturer.component';
 import { SignInComponent } from './component/sign-in/sign-in.component';
 import { HomepageComponent } from './component/homepage/homepage.component';
 import { LecturerHomepageComponent } from './component/lecturer-homepage/lecturer-homepage.component';
+import { AttendancePageComponent } from './component/attendance-page/attendance-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo:"homepage", pathMatch:"full"},
   {path:'homepage', component:HomepageComponent},
   {path:'sign-in', component:SignInComponent},
   {path:'lecturer', component: LecturerComponent},
-  {path: 'lecturerHomepage', component: LecturerHomepageComponent}
+  {path: 'lecturerHomepage', component: LecturerHomepageComponent},
+  {path: 'attendancePage', component: AttendancePageComponent}
 ];
 
 @NgModule({
