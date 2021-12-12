e9c64e5c9ff883d9fd4ab29467cb7036b2a25d1a
v-pablo <otitojuvictor9@gmail.com>
2021-12-12 20:28:25 +0100

    auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './component/homepage/homepage.component';
@@ -20,7 +20,8 @@ import { AttendancePageComponent } from './component/attendance-page/attendance-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/homepage/homepage.component.ts
+++ src/app/component/homepage/homepage.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SignInComponent } from '../sign-in/sign-in.component';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -10,16 +12,32 @@ import { SignInComponent } from '../sign-in/sign-in.component';
 export class HomepageComponent implements OnInit {
 
   
-
+  public loginForm!: FormGroup
   
 
-  constructor() { 
-    const signIn= ()=>{
-    console.log("")
-    }
-  }
+  constructor(private formbuilder: FormBuilder,private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formbuilder.group({
+      email: [''],
+      password: ['', Validators.required]
+    })
   }
-
+  login(){
+    this.http.get<any>("http://localhost:3000/signupUsers")
+    .subscribe(res=>{
+      const user = res.find((a:any)=>{
+        return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password 
+      });
+      if(user){
+        alert('Login Succesful');
+        this.loginForm.reset()
+      this.router.navigate(["home"])
+      }else{
+        alert("user not found")
+      }
+    },err=>{
+      alert("Something went wrong")
+    })
+  }
 }
